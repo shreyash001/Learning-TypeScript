b2af51aa7d928918e9a1a9b4501cc159ce29144e
Shreyash Singh <65494781+shreyash001@users.noreply.github.com>
2022-12-17 11:02:55 +0530

    Generics

--- Module 7/dist/app.js
+++ Module 7/dist/app.js
@@ -13,6 +13,19 @@ function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
 // console.log(merge({name:'Max'},{age:25}));
-const mergeObj = merge({ name: 'Max' }, { age: 25 });
-console.log(mergeObj.age);
-console.log(mergeObj.name);
+// const mergeObj = merge({name:'Max'},{age:25});
+// const mergeObj = merge({name:'Max', hobbie:['Sports']},30);
+const mergeObj = merge({ name: 'Max', hobbie: ['Sports'] }, { age: 25 });
+// console.log(mergeObj.age);
+console.log(mergeObj);
+function countAndPrint(element) {
+    let description = 'Got no Value';
+    if (element.length === 1) {
+        description = 'Got 1 Value';
+    }
+    else if (element.length > 1) {
+        description = 'Got ' + element.length + ' values';
+    }
+    return [element, description];
+}
+console.log(countAndPrint('Hi there !'));
--- Module 7/src/app.ts
+++ Module 7/src/app.ts
@@ -13,11 +13,32 @@ console.log('App is working');
 //     }, 2000);
 // })
 
-function merge<T, U>(objA:T, objB:U){
-    return Object.assign(objA,objB);
+function merge<T extends object, U extends object>(objA: T, objB:U){
+    return Object.assign(objA, objB);
 }
 
 // console.log(merge({name:'Max'},{age:25}));
-const mergeObj = merge({name:'Max'},{age:25});
-console.log(mergeObj.age);
-console.log(mergeObj.name);
+// const mergeObj = merge({name:'Max'},{age:25});
+// const mergeObj = merge({name:'Max', hobbie:['Sports']},30);
+const mergeObj = merge({name:'Max', hobbie:['Sports']},{age:25});
+
+
+// console.log(mergeObj.age);
+console.log(mergeObj);
+
+interface lengthCalculate{
+    length:number;
+}
+
+function countAndPrint<T extends lengthCalculate>(element:T): [T ,string]{
+    let description = 'Got no Value';
+    if(element.length === 1 ){
+        description = 'Got 1 Value'
+    }
+    else if(element.length > 1){
+        description = 'Got '+ element.length + ' values'
+    }
+    return [element,description];
+}
+
+console.log(countAndPrint('Hi there !'));
